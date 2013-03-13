d911d13f5e3c436c650b9e9c8f2dcda997cb9900
Mustafa Küçükşengün <mksengun@gmail.com>
2013-03-13 21:58:52 +0200

    Create Arac.java

---
+++ Arac.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+
+
+
+
+public class Arac {
+
+   private String aracAdİ;
+	    private String ad;
+	    private String soyad;
+	    private String  sifre;
+	    //private Sepet kullaniciSepeti;
+	    //private Kart kullaniciKarti;
+	    //private Adres kullaniciAdresi;
+	    //private ArrayList <History> kullaniciHistorysi;
+
+	    public Arac() {
+	        aracAdİ="";
+	        ad="";
+	        soyad="";
+	        sifre="";
+	        //kullaniciSepeti=new Sepet();
+	        //kullaniciKarti=new Kart();
+	        //kullaniciHistorysi=new ArrayList<History>();
+	    }
+	public void setAracAdİ(String userName1)
+    {
+        aracAdİ=userName1;
+    }
+    public String getAracAdİ()
+    {
+        return aracAdİ;
+    }
+    public void setAd(String ad1)
+    {
+        ad=ad1;
+    }
+    public String getAd()
+    {
+        return ad;
+    }
+    public void setSoyad(String soyad1)
+    {
+        soyad=soyad1;
+    }
+    public String getSoyad()
+    {
+        return soyad;
+    }
+    public void setSifre(String sifre1)
+    {
+        sifre=sifre1;
+    }
+    public String getSifre()
+    {
+        return sifre;
+    }
+   /* public void setKullaniciSepeti(Sepet kullaniciSepeti1)
+    {
+        kullaniciSepeti=kullaniciSepeti1;
+    }
+    public Sepet getKullaniciSepeti()
+    {
+        return kullaniciSepeti;
+    }
+    public void setKullaniciKarti(Kart kullaniciKarti1)
+    {
+        kullaniciKarti=kullaniciKarti1;
+    }
+    public Kart getKullaniciKarti()
+    {
+        return kullaniciKarti;
+    }
+    public void setAdres(Adres kullaniciAdresi1)
+    {
+        kullaniciAdresi=kullaniciAdresi1;
+    }
+    public Adres getKullaniciAdresi()
+    {
+        return kullaniciAdresi;
+    }
+    public void setKullaniciHistorysi(ArrayList<History> kullaniciHistorysi1)
+    {
+        kullaniciHistorysi=kullaniciHistorysi1;
+    }*///
+
+}
